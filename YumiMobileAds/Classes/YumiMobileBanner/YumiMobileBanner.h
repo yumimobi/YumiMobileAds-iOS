00b144235f6da2fa62f7f3bd40e77f2ce4edd5fb
JiaDingYi <wzy2010416033@163.com>
2019-06-05 18:49:45 +0800

    build banner skeleton

--- YumiMobileAds/Classes/YumiMobileBanner/YumiMobileBanner.h
+++ YumiMobileAds/Classes/YumiMobileBanner/YumiMobileBanner.h
@@ -6,11 +6,50 @@
 //
 
 #import <Foundation/Foundation.h>
+/// Represents the fixed banner ad size
+typedef NS_ENUM(NSUInteger, YumiMobileBannerSize) {
+    /// iPhone and iPod Touch ad size. Typically 320x50.
+    kYumiMobileAdViewBanner320x50 = 1 << 0,
+    /// Leaderboard size for the iPad. Typically 728x90.
+    kYumiMobileAdViewBanner728x90 = 1 << 1,
+};
+
+typedef enum : NSUInteger {
+    /// The user has granted consent for personalized ads.
+    YumiMobileGDPRConsentStatusPersonalized,
+    /// The user has granted consent for non-personalized ads.
+    YumiMobileGDPRConsentStatusNonPersonalized,
+    /// The user has neither granted nor declined consent for personalized or non-personalized ads.
+    YumiMobileGDPRConsentStatusUnknown,
+} YumiMobileGDPRConsentStatus;
 
 NS_ASSUME_NONNULL_BEGIN
+@class YumiMobileBanner;
+@protocol YumiMobileBannerDelegate <NSObject>
+@optional
+- (void)yumiMobileBannerDidReceiveAd:(YumiMobileBanner *)bannerView;
+- (void)yumiMobileBanner:(YumiMobileBanner *)bannerView didFailToReceivAdWithError:(NSError *)error;
+- (void)yumimobileBannerDidClickAd:(YumiMobileBanner *)bannerView;
+@end
 
-@interface YumiMobileBanner : NSObject
+@interface YumiMobileBanner : UIView
+/// Default is YumiMobileGDPRConsentStatusUnknown.
+@property (nonatomic, assign) YumiMobileGDPRConsentStatus gdprConsentStatus;
 
++ (instancetype) new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (void)setFrame:(CGRect)frame NS_UNAVAILABLE;
+
+/// Initializes and returns a banner view relative to the banner's superview.
+- (instancetype)initWithSSPToken:(NSString *)sspToken
+                           appID:(NSString *)appID
+                     placementID:(NSString *)placementID
+                      bannerSize:(YumiMobileBannerSize)bannerSize
+                        delegate:(id<YumiMobileBannerDelegate>)delegate
+              rootViewController:(UIViewController *)rootViewController;
+
+- (void)requestAd;
 @end
 
 NS_ASSUME_NONNULL_END
--- YumiMobileAds/Classes/YumiMobileBanner/YumiMobileBanner.m
+++ YumiMobileAds/Classes/YumiMobileBanner/YumiMobileBanner.m
@@ -6,7 +6,178 @@
 //
 
 #import "YumiMobileBanner.h"
+#import "YumiMobileRequestManager.h"
+#import <WebKit/WebKit.h>
+
+@interface YumiMobileBanner ()<WKNavigationDelegate>
+@property (nonatomic) NSString *sspToken;
+@property (nonatomic) NSString *appID;
+@property (nonatomic) NSString *placementID;
+@property (nonatomic, assign) YumiMobileBannerSize bannerSize;
+@property (nonatomic, weak) id<YumiMobileBannerDelegate> delegate;
+@property (nonatomic) UIViewController *rootViewController;
+@property (nonatomic, assign) CGSize adSize;
+
+@property (nonatomic) WKWebView *web;
+@property (nonatomic) NSString *ua;
+@end
 
 @implementation YumiMobileBanner
+- (instancetype)initWithSSPToken:(NSString *)sspToken
+                           appID:(NSString *)appID
+                     placementID:(NSString *)placementID
+                      bannerSize:(YumiMobileBannerSize)bannerSize
+                        delegate:(id<YumiMobileBannerDelegate>)delegate
+              rootViewController:(UIViewController *)rootViewController {
+    self = [super init];
+    
+    self.gdprConsentStatus = YumiMobileGDPRConsentStatusUnknown;
+    self.sspToken = sspToken ?: @"";
+    self.appID = appID ?: @"";
+    self.placementID = placementID ?: @"";
+    self.delegate = delegate;
+    self.rootViewController = rootViewController;
+    // adSize
+    self.adSize = CGSizeMake(320, 50);
+    if (bannerSize == kYumiMobileAdViewBanner728x90) {
+        self.adSize = CGSizeMake(728, 90);
+    }
+    // ua
+    self.web = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, self.adSize.width, self.adSize.height)];
+    __weak __typeof(self)weakSelf = self;
+    [self.web evaluateJavaScript:@"navigator.userAgent" completionHandler:^(id result, NSError *error) {
+        weakSelf.ua = result ?: @"";
+    }];
+    return self;
+}
 
+- (void)requestAd {
+    NSString *gdprConsentStatus = @"";
+    if (self.gdprConsentStatus == YumiMobileGDPRConsentStatusPersonalized) {
+        gdprConsentStatus = @"yes";
+    }
+    if (self.gdprConsentStatus == YumiMobileGDPRConsentStatusNonPersonalized) {
+        gdprConsentStatus = @"no";
+    }
+    
+    // request model
+    YumiMobileRequestModel *requestModel =
+    [[YumiMobileRequestModel alloc]
+     initRequestModelWithSSPToken:self.sspToken
+                            appID:self.appID
+                      placementID:self.placementID
+                           adType:kYumiMobileAdBanner
+                           adSize:self.adSize
+                gdprConsentStatus:gdprConsentStatus];
+    // request server
+    [[YumiMobileRequestManager sharedManager]
+     requestAdWithRequestModel:requestModel
+                            ua:self.ua
+                       success:^(YumiMobileResponseModel * _Nonnull ad) {
+                           
+                      }failure:^(NSError * _Nonnull error) {
+        
+    }];
+    
+}
+
+#pragma mark - WKNavigationDelegate
+/*! @abstract Decides whether to allow or cancel a navigation.
+ @param webView The web view invoking the delegate method.
+ @param navigationAction Descriptive information about the action
+ triggering the navigation request.
+ @param decisionHandler The decision handler to call to allow or cancel the
+ navigation. The argument is one of the constants of the enumerated type WKNavigationActionPolicy.
+ @discussion If you do not implement this method, the web view will load the request or, if appropriate, forward it to another application.
+ */
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+}
+
+/*! @abstract Decides whether to allow or cancel a navigation after its
+ response is known.
+ @param webView The web view invoking the delegate method.
+ @param navigationResponse Descriptive information about the navigation
+ response.
+ @param decisionHandler The decision handler to call to allow or cancel the
+ navigation. The argument is one of the constants of the enumerated type WKNavigationResponsePolicy.
+ @discussion If you do not implement this method, the web view will allow the response, if the web view can show it.
+ */
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    
+}
+
+/*! @abstract Invoked when a main frame navigation starts.
+ @param webView The web view invoking the delegate method.
+ @param navigation The navigation.
+ */
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+/*! @abstract Invoked when a server redirect is received for the main
+ frame.
+ @param webView The web view invoking the delegate method.
+ @param navigation The navigation.
+ */
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+/*! @abstract Invoked when an error occurs while starting to load data for
+ the main frame.
+ @param webView The web view invoking the delegate method.
+ @param navigation The navigation.
+ @param error The error that occurred.
+ */
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+    
+}
+
+/*! @abstract Invoked when content starts arriving for the main frame.
+ @param webView The web view invoking the delegate method.
+ @param navigation The navigation.
+ */
+- (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+/*! @abstract Invoked when a main frame navigation completes.
+ @param webView The web view invoking the delegate method.
+ @param navigation The navigation.
+ */
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+/*! @abstract Invoked when an error occurs during a committed main frame
+ navigation.
+ @param webView The web view invoking the delegate method.
+ @param navigation The navigation.
+ @param error The error that occurred.
+ */
+- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+    
+}
+
+/*! @abstract Invoked when the web view needs to respond to an authentication challenge.
+ @param webView The web view that received the authentication challenge.
+ @param challenge The authentication challenge.
+ @param completionHandler The completion handler you must invoke to respond to the challenge. The
+ disposition argument is one of the constants of the enumerated type
+ NSURLSessionAuthChallengeDisposition. When disposition is NSURLSessionAuthChallengeUseCredential,
+ the credential argument is the credential to use, or nil to indicate continuing without a
+ credential.
+ @discussion If you do not implement this method, the web view will respond to the authentication challenge with the NSURLSessionAuthChallengeRejectProtectionSpace disposition.
+ */
+- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler {
+    
+}
+
+/*! @abstract Invoked when the web view's web content process is terminated.
+ @param webView The web view whose underlying web content process was terminated.
+ */
+- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView API_AVAILABLE(macosx(10.11), ios(9.0)) {
+    
+}
 @end
