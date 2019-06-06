aaa7a1e36284a66fda0f41df112143499aec0856
JiaDingYi <wzy2010416033@163.com>
2019-06-06 11:30:13 +0800

    build request model

--- YumiMobileAds/Classes/YumiMobileAPI/YumiMobileRequestManager.m
+++ YumiMobileAds/Classes/YumiMobileAPI/YumiMobileRequestManager.m
@@ -6,7 +6,47 @@
 //
 
 #import "YumiMobileRequestManager.h"
+#import "YMNetworking.h"
+#import "YumiMobileRequestModel.h"
+#import "YumiMobileResponseModel.h"
+#import "YMModel.h"
+
+@interface YumiMobileRequestManager ()
+@property (nonatomic) YMHTTPSessionManager *client;
+@property (nonatomic) NSString *ua;
+
+@end
 
 @implementation YumiMobileRequestManager
++ (instancetype)sharedManager {
+    static YumiMobileRequestManager *sharedManager;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[YumiMobileRequestManager alloc] init];
+    });
+    
+    return sharedManager;
+}
 
+- (YMHTTPSessionManager *)client {
+    if (!_client) {
+        _client = [YMHTTPSessionManager manager];
+        _client.requestSerializer.timeoutInterval = 15;
+        _client.responseSerializer = [YMJSONResponseSerializer serializer];
+    }
+    
+    [_client.requestSerializer setValue:self.ua forHTTPHeaderField:@"User-Agent"];
+    return _client;
+}
+
+- (void)requestAdWithRequestModel:(YumiMobileRequestModel *)model
+                               ua:(NSString *)ua
+                          success:(void (^)(YumiMobileResponseModel *ad))success
+                          failure:(void (^)(NSError *error))failure {
+    self.ua = ua;
+    YMHTTPSessionManager *client = self.client;
+    
+    NSString *parameters = [model ym_modelToJSONObject];
+    
+}
 @end
--- YumiMobileAds/Classes/YumiMobileAPI/YumiMobileRequestModel.m
+++ YumiMobileAds/Classes/YumiMobileAPI/YumiMobileRequestModel.m
@@ -6,7 +6,31 @@
 //
 
 #import "YumiMobileRequestModel.h"
+#import "YumiMobileTools.h"
+
 @interface YumiMobileRequestModel ()
+@property (nonatomic) NSString *protocolVersion;
+@property (nonatomic) NSString *sspToken;
+@property (nonatomic) NSString *appID;
+@property (nonatomic) NSString *appName;
+@property (nonatomic) NSString *bundleID;
+@property (nonatomic) NSString *model;
+@property (nonatomic) NSString *make;
+@property (nonatomic) NSString *connectionType;
+@property (nonatomic) NSString *carrier; // int
+@property (nonatomic) NSString *orientation; // int
+@property (nonatomic) NSString *idfa;
+@property (nonatomic) NSString *osType;
+@property (nonatomic) NSString *osVersion;
+@property (nonatomic) int screenW;
+@property (nonatomic) int screenH;
+@property (nonatomic) int adType;
+@property (nonatomic) NSString *placementID;
+@property (nonatomic) int adWidth;
+@property (nonatomic) int adHeight;
+// 创意类型
+// 1.图片，2.图文，4.html,5.文本
+@property (nonatomic) NSArray *inventoryTypes;
 
 @end
 
@@ -18,8 +42,56 @@
                                       adSize:(CGSize)adSize
                            gdprConsentStatus:(NSString *)consentStatus {
     self = [super init];
+    YumiMobileTools *tool = [YumiMobileTools sharedTool];
     
+    self.protocolVersion = @"1.1";
+    self.sspToken = sspToken;
+    self.appID = appID;
+    self.appName = tool.appName;
+    self.bundleID = tool.bundleID;
+    self.model = tool.model;
+    self.make = @"Apple";
+    self.connectionType = tool.networkStatus;
+    self.carrier = tool.carrierName;
+    self.orientation = tool.isInterfaceOrientationPortrait ? @"1" : @"3";
+    self.idfa = tool.idfa;
+    self.osType = @"iOS";
+    self.osVersion = tool.systemVersion;
+    self.screenW = [UIScreen mainScreen].bounds.size.width;
+    self.screenH = [UIScreen mainScreen].bounds.size.height;
+    self.adType = adType;
+    self.placementID = placementID;
+    self.adWidth = adSize.width;
+    self.adHeight = adSize.height;
+    self.inventoryTypes = @[@1,@2,@4,@5];
     return self;
+}
+
++ (NSDictionary *)modelCustomPropertyMapper {
+    return @{@"protocolVersion" : @"ver",
+             @"sspToken" : @"ssp_token",
+             @"appID" : @"app.id",
+             @"appName" : @"app.name",
+             @"bundleID" : @"app.bundle",
+             @"model" : @"device.model",
+             @"make" : @"device.make",
+             @"connectionType" : @"device.connection_type",
+             @"carrier" : @"device.carrier",
+             @"orientation" : @"device.orientation",
+             @"idfa" : @"device.ios_adid",
+             @"osType" : @"device.os_type",
+             @"screenW" : @"device.screen.w",
+             @"screenH" : @"device.screen.h",
+             @"adType" : @"ads.type",
+             @"placementID" : @"ads.place_id",
+             @"adWidth" : @"ads.w",
+             @"adHeight" : @"ads.h",
+             @"inventoryTypes" : @"ads.inventory_types",
+             };
+}
+
+- (BOOL)modelCustomTransformToDictionary:(NSMutableDictionary *)dic {
+    return YES;
 }
 
 @end
