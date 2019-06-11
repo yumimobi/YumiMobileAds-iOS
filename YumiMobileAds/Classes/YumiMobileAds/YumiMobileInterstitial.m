98acbe5c29979439b016e89cde821c99275f6402
JiaDingYi <wzy2010416033@163.com>
2019-06-11 12:27:02 +0800

    create yumi mobile interstitial

---
+++ YumiMobileAds/Classes/YumiMobileAds/YumiMobileInterstitial.m
@@ -0,0 +1,12 @@
+//
+//  YumiMobileInterstitial.m
+//  YumiMobileAds
+//
+//  Created by 王泽永 on 2019/6/11.
+//
+
+#import "YumiMobileInterstitial.h"
+
+@implementation YumiMobileInterstitial
+
+@end
