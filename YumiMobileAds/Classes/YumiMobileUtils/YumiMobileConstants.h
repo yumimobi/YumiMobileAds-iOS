33c8f9ab1604f7b430e185eae5a75f99837df8ec
JiaDingYi <wzy2010416033@163.com>
2019-06-11 12:27:12 +0800

    finetune public header

--- YumiMobileAds/Classes/YumiMobileUtils/YumiMobileConstants.h
+++ YumiMobileAds/Classes/YumiMobileUtils/YumiMobileConstants.h
@@ -13,4 +13,20 @@
 #define SYSTEM_VERSION_LESS_THAN(v)                                                                                    \
 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 
+typedef enum : NSUInteger {
+    /// The user has granted consent for personalized ads.
+    YumiMobileGDPRConsentStatusPersonalized,
+    /// The user has granted consent for non-personalized ads.
+    YumiMobileGDPRConsentStatusNonPersonalized,
+    /// The user has neither granted nor declined consent for personalized or non-personalized ads.
+    YumiMobileGDPRConsentStatusUnknown,
+} YumiMobileGDPRConsentStatus;
+
+/// Represents the fixed banner ad size
+typedef NS_ENUM(NSUInteger, YumiMobileBannerSize) {
+    /// iPhone and iPod Touch ad size. Typically 320x50.
+    kYumiMobileAdViewBanner320x50 = 1 << 0,
+    /// Leaderboard size for the iPad. Typically 728x90.
+    kYumiMobileAdViewBanner728x90 = 1 << 1,
+};
 #endif /* YumiMobileConstants_h */
