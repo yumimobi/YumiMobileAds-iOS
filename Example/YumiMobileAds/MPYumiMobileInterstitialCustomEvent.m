534c03afe3c6e17eff0b3dfa3c0c55d7fca24319
JiaDingYi <wzy2010416033@163.com>
2019-06-11 19:51:18 +0800

    create interstitial custom event

---
+++ Example/YumiMobileAds/MPYumiMobileInterstitialCustomEvent.m
@@ -0,0 +1,13 @@
+//
+//  MPYumiMobileInterstitialCustomEvent.m
+//  YumiMobileAds_Example
+//
+//  Created by 王泽永 on 2019/6/11.
+//  Copyright © 2019 wzy2010416033@163.com. All rights reserved.
+//
+
+#import "MPYumiMobileInterstitialCustomEvent.h"
+
+@implementation MPYumiMobileInterstitialCustomEvent
+
+@end
