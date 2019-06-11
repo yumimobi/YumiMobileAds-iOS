4bc042d42161017a6f27e1275a981eb8393f7ac9
JiaDingYi <wzy2010416033@163.com>
2019-06-11 17:49:47 +0800

    add interstitial enum

--- YumiMobileAds/Classes/YumiMobileUtils/YumiMobileConstants.h
+++ YumiMobileAds/Classes/YumiMobileUtils/YumiMobileConstants.h
@@ -29,4 +29,14 @@ typedef NS_ENUM(NSUInteger, YumiMobileBannerSize) {
     /// Leaderboard size for the iPad. Typically 728x90.
     kYumiMobileAdViewBanner728x90 = 1 << 1,
 };
+
+typedef enum : NSUInteger {
+    /// Default is 300*250 in landscape,250*300 in portrait.
+    kYumiMobileAdInterstitial300x250,
+    /// Default is 600*500 in landscape,500*600 in portrait.
+    kYumiMobileAdInterstitial600x500,
+    /// Default is 960*640 in landscape,640*900 in portrait.
+    kYumiMobileAdInterstitial960x640,
+} YumiMobileInterstitialSize;
+
 #endif /* YumiMobileConstants_h */
