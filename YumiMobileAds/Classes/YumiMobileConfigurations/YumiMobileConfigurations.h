3f52c92340365d104c162da42efb680c1d79eef9
JiaDingYi <wzy2010416033@163.com>
2019-06-12 16:03:57 +0800

    create yumi mobile configuration

---
+++ YumiMobileAds/Classes/YumiMobileConfigurations/YumiMobileConfigurations.h
@@ -0,0 +1,16 @@
+//
+//  YumiMobileConfigurations.h
+//  YumiMobileAds
+//
+//  Created by 王泽永 on 2019/6/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiMobileConfigurations : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YumiMobileAds/Classes/YumiMobileConfigurations/YumiMobileConfigurations.m
@@ -0,0 +1,12 @@
+//
+//  YumiMobileConfigurations.m
+//  YumiMobileAds
+//
+//  Created by 王泽永 on 2019/6/12.
+//
+
+#import "YumiMobileConfigurations.h"
+
+@implementation YumiMobileConfigurations
+
+@end
