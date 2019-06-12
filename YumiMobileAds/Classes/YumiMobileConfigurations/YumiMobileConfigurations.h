698a3e7b5e9a0ff1f586d7c18f756c69b8268c4d
JiaDingYi <wzy2010416033@163.com>
2019-06-12 16:04:18 +0800

    build configuration logic

--- YumiMobileAds/Classes/YumiMobileConfigurations/YumiMobileConfigurations.h
+++ YumiMobileAds/Classes/YumiMobileConfigurations/YumiMobileConfigurations.h
@@ -10,6 +10,17 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface YumiMobileConfigurations : NSObject
+// Indicates the GDPR requirement of the user. If it's 1, the user's subject to the GDPR laws.
+// Default is 0.
+@property (nonatomic, assign) int gdpr;
+// Your user's consent string. In this case, the user has given consent to store and process personal information.
+// Default is true.
+@property (nonatomic) BOOL consent;
+// Indicates the coppa requirement of the user. If it's 1, the user's subject to the coppa laws.
+// Default is 1.
+@property (nonatomic, assign) int coppa;
+
++ (instancetype)sharedYumiMobileConfigurations;
 
 @end
 
--- YumiMobileAds/Classes/YumiMobileConfigurations/YumiMobileConfigurations.m
+++ YumiMobileAds/Classes/YumiMobileConfigurations/YumiMobileConfigurations.m
@@ -8,5 +8,21 @@
 #import "YumiMobileConfigurations.h"
 
 @implementation YumiMobileConfigurations
++ (instancetype)sharedYumiMobileConfigurations {
+    static YumiMobileConfigurations *_instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _instance = [[self alloc] init];
+    });
+    return _instance;
+}
 
+- (instancetype)init {
+    self = [super init];
+    
+    self.gdpr = 0;
+    self.consent = true;
+    self.coppa = 1;
+    return self;
+}
 @end
